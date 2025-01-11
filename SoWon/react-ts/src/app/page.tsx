e633241eca23b516c2b1d26eae9be28fc0889559
Sowon Kim <sso626262@naver.com>
2025-01-11 18:21:32 +0900

    feat: 일기 작성 컴포넌트 추가 (#3)

--- SoWon/react-ts/src/app/page.tsx
+++ SoWon/react-ts/src/app/page.tsx
@@ -1,3 +1,24 @@
+const DiaryWriter = () => {
+    return (
+        <>
+            <input placeholder="제목을 적어보세요" />
+            <div>
+                <button>bad</button>
+                <button>soso</button>
+                <button>good</button>
+                <button>great</button>
+                <button>awesome</button>
+            </div>
+            <textarea placeholder="오늘 당신의 하루는 어땠나요?"></textarea>
+            <button>일기를 더 자세히 적어볼까요?</button>
+        </>
+    )
+}
+
 export default function DiaryHomePage() {
-    return <>Diary Home</>
+    return (
+        <>
+            <DiaryWriter />
+        </>
+    )
 }
