02ed39a848b3faf83fd646fbd42d12f2a347970c
causyj <jainayj804998@gmail.com>
2025-01-12 14:15:10 +0900

    feat: add emotions

--- YuJin/react-ts/src/app/layout.tsx
+++ YuJin/react-ts/src/app/layout.tsx
@@ -1,3 +1,7 @@
 export const RootLayout = ({ children }: React.PropsWithChildren) => {
-    return <main className="min-h-screen max-h-screen h-screen w-full bg-gray-50">{children}</main>
+    return (
+        <main className="min-h-screen max-h-screen h-screen w-full bg-white flex items-center justify-center">
+            {children}
+        </main>
+    )
 }
--- YuJin/react-ts/src/app/page.tsx
+++ YuJin/react-ts/src/app/page.tsx
@@ -1,3 +1,27 @@
 export default function DiaryHomePage() {
-    return <>Diary Home</>
+    const emotions: string[] = ['bad', 'soso', 'good', 'great', 'awesome']
+    const emotionEntries = emotions.map((emotion) => ({
+        emotion: emotion,
+    }))
+    return (
+        <div className="flex flex-col gap-10 items-center justify-center">
+            <div className="w-full border-2 border-bg-gray px-4 rounded-lg">
+                <div className="py-8">제목을 적어보세요</div>
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-row gap-1">
+                        {emotionEntries.map((entry, index) => (
+                            <div className="flex bg-gray-400 px-2 py-0.5 items-center rounded-lg" key={index}>
+                                {entry.emotion}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex flex-row gap-1">
+                        <div className="flex bg-gray-400 px-2 py-0.5 items-center rounded-lg">bad</div>
+                        <div className=" bg-gray-400 p-2">bad</div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full border-2 border-bg-gray px-4 rounded-lg">기록된 일기</div>
+        </div>
+    )
 }
