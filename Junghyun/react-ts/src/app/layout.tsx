57fd12ff4704ac4d33c350e2f72bb941321debd6
katie424 <7080katie@gmail.com>
2025-01-15 21:53:49 +0900

    feat: add base layout

--- Junghyun/react-ts/src/app/layout.tsx
+++ Junghyun/react-ts/src/app/layout.tsx
@@ -1,3 +1,7 @@
 export const RootLayout = ({ children }: React.PropsWithChildren) => {
-    return <main className="min-h-screen max-h-screen h-screen w-full bg-gray-50">{children}</main>
+    return (
+        <main className="min-h-screen max-h-screen h-screen w-full flex items-center justify-center bg-white">
+            {children}
+        </main>
+    )
 }
