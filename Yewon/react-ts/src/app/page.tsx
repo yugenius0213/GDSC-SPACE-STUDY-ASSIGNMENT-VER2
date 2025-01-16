03a565150df0cf10a197dd8560d4df8cae6c3e7d
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2025-01-16 22:28:43 +0900

    feat: add weather buttons

--- Yewon/react-ts/src/app/page.tsx
+++ Yewon/react-ts/src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Diary } from '../interface/diary'
 
 type Emotion = Diary['emotion']
+type Weather = Diary['weather']
 
 const emotions: Emotion[] = ['bad', 'soso', 'good', 'great', 'awesome']
+const weathers: Weather[] = ['sunny', 'cloud', 'rain', 'snow']
 
 const EmotionIcon = ({ emotion }: { emotion: Diary['emotion'] }) => {
     const icons: Record<Diary['emotion'], string> = {
@@ -18,7 +20,7 @@ const EmotionIcon = ({ emotion }: { emotion: Diary['emotion'] }) => {
 const DiaryWriter = () => {
     return (
         <div className="flex-col gap-4 p-4 rounded-lg bg-white border border-gray-100 h-2/3">
-            <input required type="text" className="inputfield-text text-2xl" placeholder="제목을 적어보세요." />
+            <input required type="text" className="inputfield-text mt-4 text-2xl" placeholder="제목을 적어보세요." />
             <div className="flex mt-6 gap-2">
                 {emotions.map((emotion, index) => (
                     <button key={index} type="button" className="default-btn" name="feeling" value={emotion}>
@@ -26,9 +28,16 @@ const DiaryWriter = () => {
                     </button>
                 ))}
             </div>
+            <div className="flex mt-2 mb-6 gap-2">
+                {weathers.map((weather,index)=>(
+                    <button key={index} type="button" className="default-btn" name="weather" value={weather}>
+                        {weather}
+                    </button>
+                ))}
+            </div>
             <textarea
                 required
-                className="inputfield-text resize-none h-1/2"
+                className="inputfield-text resize-none h-1/2 mb-2"
                 placeholder="오늘 당신의 하루는 어땠나요?"
             />
         </div>
