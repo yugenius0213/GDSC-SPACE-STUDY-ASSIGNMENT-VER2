cacdbf535038b6fdb5e1e32bace10bbf1212fb95
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2025-01-16 21:04:25 +0900

    feat: add text fields

--- Yewon/react-ts/src/app/page.tsx
+++ Yewon/react-ts/src/app/page.tsx
@@ -1,3 +1,20 @@
+const DiaryWriter = () => {
+    return (
+        <div className="flex-col gap-4 p-4 rounded-lg bg-white border border-gray-100 h-2/3">
+            <input required type="text" className="inputfield-text text-2xl" placeholder="제목을 적어보세요." />
+            <textarea
+                required
+                className="inputfield-text resize-none h-1/2"
+                placeholder="오늘 당신의 하루는 어땠나요?"
+            />
+        </div>
+    )
+}
+
 export default function DiaryHomePage() {
-    return <>Diary Home</>
+    return (
+        <div className="flex flex-col items-center justify-center gap-10 h-full md:grid md:grid-rows-1 md:grid-cols-[3fr,2fr] md:w-4/5 lg:w-2/3">
+            <DiaryWriter/>
+        </div>
+    )
 }
