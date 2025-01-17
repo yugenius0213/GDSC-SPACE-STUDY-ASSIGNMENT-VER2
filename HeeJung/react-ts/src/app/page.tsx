e24ed7a387c8abb479736ac1fc44a97136acc37e
heejung <gimm9567@gmail.com>
2025-01-18 02:41:33 +0900

    feat: add link

--- HeeJung/react-ts/src/app/page.tsx
+++ HeeJung/react-ts/src/app/page.tsx
@@ -69,6 +69,12 @@ function DiaryRecord() {
         <div className="w-full flex flex-col items-start gap-4 p-4 justify-between rounded-lg bg-white border border-gray-100 h-2/3 min-h-[20rem]">
             <h1 className="text-xl mt-5 text-emerald-600">기록된 일기</h1>
             <div className="flex items-center justify-center text-gray-400">일기를 적어보세요</div>
+            <Link
+                to="/emotions"
+                className="flex items-center justify-center rounded-lg border border-transparent active:translate-y-[1px] w-full p-2 cursor-pointer transition-colors ease-in bg-emerald-100 text-emerald-600 hover:border-emerald-600 hover:text-emerald-600"
+            >
+                감정 모아보기
+            </Link>
         </div>
     )
 }
