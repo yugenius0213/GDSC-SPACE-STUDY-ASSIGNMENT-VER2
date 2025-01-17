59da784978ea6f97312bb55e6a61bbe4c35ad914
causyj <jainayj804998@gmail.com>
2025-01-17 23:56:33 +0900

    feat: add diary detail interface

--- YuJin/react-ts/src/app/constants/diaryOutput.ts
+++ YuJin/react-ts/src/app/constants/diaryOutput.ts
@@ -1,4 +1,5 @@
 export const DIARY_LIST_TITLE = '기록된 일기'
+export const EMPTY_DIARY = '일기를 적어보세요'
 export const VIEW_EMOTIONS_BUTTON_TEXT = '감정 모아보기'
 export const GO_YO_WRITE_DIARY = '새로운 일기 쓰러 가기'
 export const DELETE_CURRENT_DIARY = '현재 일기 삭제하기'
