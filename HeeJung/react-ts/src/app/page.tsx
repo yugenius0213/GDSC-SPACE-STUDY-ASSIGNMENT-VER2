aa9cf46dbe8bccb1a2dff0e679631392d166f793
heejung <gimm9567@gmail.com>
2025-01-18 02:34:59 +0900

    feat: declare variant

--- HeeJung/react-ts/src/app/page.tsx
+++ HeeJung/react-ts/src/app/page.tsx
@@ -1,5 +1,11 @@
 function DiaryWriter() {
-    
+    const [title, setTitle] = useState<string>('')
+    const [emotion, setEmotion] = useState<Diary['emotion'] | undefined>()
+    const [weather, setWeather] = useState<Diary['weather'] | undefined>()
+    const emotions: Diary['emotion'][] = ['bad', 'soso', 'good', 'great', 'awesome']
+    const weathers: Diary['weather'][] = ['cloud', 'rain', 'snow', 'sunny']
+    const [contents, setContents] = useState<string>('')
+    const [isValid, setIsValid] = useState(false)
 }
 
 function DiaryRecord() {
