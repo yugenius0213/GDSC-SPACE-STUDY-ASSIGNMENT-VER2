b0370ccc1afbe264359280d315a9a4687a3e5992
causyj <jainayj804998@gmail.com>
2025-01-23 19:44:07 +0900

    feat: add additional emotion data

--- YuJin/react-ts/src/app/constants/emotions.ts
+++ YuJin/react-ts/src/app/constants/emotions.ts
@@ -1,24 +1,38 @@
 import { Diary } from '../../interface/diary'
 
-export const EMOTION_DATA: Record<
-    Diary['emotion'],
-    {
-        emojio: string
-    }
-> = {
+export const EMOTION_DATA: Readonly<
+    Record<
+        Diary['emotion'],
+        {
+            emojio: string
+            name_eng: string
+            name_kor: string
+        }
+    >
+> = Object.freeze({
     awesome: {
         emojio: '😎',
+        name_eng: 'Awesome',
+        name_kor: '최고의 하루였어요',
     },
     bad: {
         emojio: '🤬',
+        name_eng: 'Bad',
+        name_kor: '최악의 하루였어요!',
     },
     great: {
         emojio: '😃',
+        name_eng: 'Great',
+        name_kor: '멋진 하루였어요',
     },
     good: {
         emojio: '😙',
+        name_eng: 'Good',
+        name_kor: '좋은 하루였어요',
     },
     soso: {
         emojio: '😗',
+        name_eng: 'Soso',
+        name_kor: '괜찮은 하루였어요',
     },
-}
+})
