9577d760384faaa6cb143cd8978abeb1befc4313
causyj <jainayj804998@gmail.com>
2025-01-24 21:18:46 +0900

    feat: edit emotion button color for grouping

--- YuJin/react-ts/src/app/constants/emotions.ts
+++ YuJin/react-ts/src/app/constants/emotions.ts
@@ -7,6 +7,7 @@ export const EMOTION_DATA: Readonly<
             emoji: string
             name: string
             description: string
+            color: string
         }
     >
 > = Object.freeze({
@@ -14,27 +15,32 @@ export const EMOTION_DATA: Readonly<
         emoji: '😎',
         name: 'Awesome',
         description: '최고의 하루였어요',
+        color: 'yellow',
     },
 
     great: {
         emoji: '😃',
         name: 'Great',
         description: '멋진 하루였어요',
+        color: 'blue',
     },
     good: {
         emoji: '😙',
         name: 'Good',
         description: '좋은 하루였어요',
+        color: 'green',
     },
     soso: {
         emoji: '😗',
         name: 'Soso',
         description: '괜찮은 하루였어요',
+        color: 'purple',
     },
     bad: {
         emoji: '🤬',
         name: 'Bad',
         description: '최악의 하루였어요!',
+        color: 'red',
     },
 })
 export const EMOTION_LIST = Object.entries(EMOTION_DATA).map(([key, value]) => ({
--- YuJin/react-ts/src/app/emotions/page.tsx
+++ YuJin/react-ts/src/app/emotions/page.tsx
@@ -1,5 +1,16 @@
 import { EMOTION_LIST } from '../constants/emotions'
 
+type EmotionColorVariants = {
+    [key: string]: string
+}
+
+const emotionColorVariants: EmotionColorVariants = {
+    yellow: 'bg-[#FEFCE8] border border-[#fef9c2] group-hover:bg-[#fef9c2] group-hover:border-[#fff085]',
+    blue: 'bg-[#eff6ff] border border-[#dbeafe] group-hover:bg-[#dbeafe] group-hover:border-[#bedbff]',
+    green: 'bg-[#f0fdf4] border border-[#dcfce7] group-hover:bg-[#dcfce7] group-hover:border-[#b9f8cf]',
+    purple: 'bg-[#faf5ff] border border-[#f3e8ff] group-hover:bg-[#f3e8ff] group-hover:border-[#e9d4ff]',
+    red: 'bg-[#fef2f2] border border-[#ffe2e2] group-hover:bg-[#ffe2e2] group-hover:border-[#ffc9c9]',
+}
 export default function EmotionLinkPage() {
     return (
         <div className="flex flex-col gap-10">
@@ -9,10 +20,12 @@ export default function EmotionLinkPage() {
             </div>
             <div className="grid grid-cols-2 gap-5">
                 {EMOTION_LIST.map((emotion, index) => (
-                    <div key={index} className="flex flex-row gap-4 border p-3 rounded-2xl">
+                    <div
+                        key={index}
+                        className="flex flex-row gap-4 border border-primary-lightgray hover:border-transparent p-3 rounded-2xl hover:shadow-2xl hover:scale-105 active:scale-100 transition-all ease-out duration-150 group"
+                    >
                         <div
-                            className={`flex items-center justify-center  bg-${emotion.emotion} border border-${emotion.emotion}-border 
-                hover:bg-${emotion.emotion}-hover hover:border-${emotion.emotion}-hover-border w-24 h-24 rounded-2xl text-6xl`}
+                            className={`flex items-center justify-center group-hover:shadow-inner w-24 h-24 min-w-[6rem] min-h-[6rem] rounded-2xl text-6xl ${emotionColorVariants[emotion.color]}`}
                         >
                             {emotion.emoji}
                         </div>
