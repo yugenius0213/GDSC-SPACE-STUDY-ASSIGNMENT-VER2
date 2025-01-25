308b6caa73587eb74ddccfc1281138070dd3d8ed
causyj <jainayj804998@gmail.com>
2025-01-25 12:27:21 +0900

    feat: get emotion data

--- YuJin/react-ts/src/app/emotions/[emotion]/page.tsx
+++ YuJin/react-ts/src/app/emotions/[emotion]/page.tsx
@@ -1,9 +1,23 @@
 import { useParams } from 'react-router-dom'
+import { emotionColorVariants } from '../../../styles/emotionCard'
+import { EMOTION_DATA } from '../../constants/emotions'
+import { Diary } from '../../../interface/diary'
 
 type EmotionPageParams = {
-    emotion: string
+    emotion: Diary['emotion']
 }
 export default function EmotionPage() {
     const { emotion } = useParams<EmotionPageParams>()
-    return <>{emotion} Emotion page </>
+    return (
+        <div className="gap-4">
+            <div className="flex flex-row gap-4 items-center justify-center">
+                <div
+                    className={`flex items-center justify-center group-hover:shadow-inner ${emotionColorVariants[EMOTION_DATA[emotion!].color]} w-24 h-24 min-w-[6rem] min-h-[6rem] rounded-2xl text-6xl`}
+                >
+                    {EMOTION_DATA[emotion!].emoji}
+                </div>
+                <h1 className="text-3xl font-semibold">{EMOTION_DATA[emotion!].description}</h1>
+            </div>
+        </div>
+    )
 }
