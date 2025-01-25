a9d760e5a8fc5c5e96321d12c0a8ea9086ed0f83
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2025-01-25 18:37:43 +0900

    feat: add date parsing logic

--- Yewon/react-ts/src/app/detail/[id]/page.tsx
+++ Yewon/react-ts/src/app/detail/[id]/page.tsx
@@ -12,16 +12,27 @@ export default function DiaryDetailPage() {
     const storedData: Diary[] = JSON.parse(localStorage.getItem(DIARYKEY)!) || []
     const [diary, setDiary] = useState<Diary | undefined>()
 
+    const formatDate = (date: Date): string => {
+        const strDate = date.toString()
+
+        const year = strDate.substring(0, 4)
+        const month = strDate.substring(5, 7)
+        const day = strDate.substring(8, 10)
+        return `${year}년 ${month}월 ${day}일`
+    } //DiaryCard의 formatData와 합치기
+
     useEffect(() => {
         setDiary(storedData.find((item) => item.id === id))
-    }, [id])
+    }, [])
 
     return (
         <div className="w-2/4 h-full py-20">
             <div className="flex flex-col gap-4 my-9">
                 <h1 className="text-4xl font-medium">{diary?.title}</h1>
                 <div className="flex flex-row gap-2">
-                    <button className="grow default-btn">2025년 1월 25일</button>
+                    <button className="grow default-btn">
+                        {diary?.date ? formatDate(diary.date) : '날짜를 불러올 수 없습니다.'}
+                    </button>
                     <button className="grow default-btn">{diary?.weather}</button>
                     <button className="grow default-btn">{diary?.emotion}</button>
                 </div>
