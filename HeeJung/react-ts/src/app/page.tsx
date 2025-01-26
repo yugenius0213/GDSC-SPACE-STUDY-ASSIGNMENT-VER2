f9ed89521f4b825b3ec96cdc8a26f695e9a57e62
heejung <gimm9567@gmail.com>
2025-01-27 01:41:44 +0900

    feat: implement diary record

--- HeeJung/react-ts/src/app/page.tsx
+++ HeeJung/react-ts/src/app/page.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import { useDiaryUpdate, useDiaryValue } from '../provider/Diary'
 import { Diary } from '../interface/diary'
-import { Link } from 'react-router-dom'
+import { Link } from '../components/Link'
+import { EMOTION } from './constants/emotion'
+import { WEATHER } from './constants/weather'
 
 function DiaryWriter() {
     const [title, setTitle] = useState<string>('')
@@ -59,8 +61,12 @@ function DiaryWriter() {
                 <div className="flex flex-row gap-1">
                     {emotions.map((selectedEmotion, index) => (
                         <button
-                            className={`flex items-center justify-center rounded-lg border border-transparent active:translate-y-[1px] p-2 cursor-pointer transition-colors ease-in px-1.5 py-0.5 text-sm ${emotion == selectedEmotion ? 'bg-emerald-100 text-emerald-600 hover:border-emerald-600 hover:text-emerald-600' : 'bg-gray-100 text-gray-400 hover:border-gray-600 hover:text-gray-600'}`}
                             key={index}
+                            className={`flex items-center justify-center rounded-lg border border-transparent active:translate-y-[1px] p-2 cursor-pointer transition-colors ease-in px-1.5 py-0.5 text-sm ${
+                                emotion === selectedEmotion
+                                    ? 'bg-emerald-100 text-emerald-600 hover:border-emerald-600 hover:text-emerald-600'
+                                    : 'bg-gray-100 text-gray-400 hover:border-gray-600 hover:text-gray-600'
+                            }`}
                             onClick={() => setEmotion(selectedEmotion)}
                         >
                             {selectedEmotion}
@@ -70,8 +76,12 @@ function DiaryWriter() {
                 <div className="flex flex-row gap-1">
                     {weathers.map((selectedWeather, index) => (
                         <button
-                            className={`flex items-center justify-center rounded-lg border border-transparent active:translate-y-[1px] p-2 cursor-pointer transition-colors ease-in px-1.5 py-0.5 text-sm ${weather == selectedWeather ? 'bg-blue-100 text-blue-600 hover:border-blue-600 hover:text-blue-600' : 'bg-gray-100 text-gray-400 hover:border-gray-600 hover:text-gray-600'}`}
                             key={index}
+                            className={`flex items-center justify-center rounded-lg border border-transparent active:translate-y-[1px] p-2 cursor-pointer transition-colors ease-in px-1.5 py-0.5 text-sm ${
+                                weather === selectedWeather
+                                    ? 'bg-blue-100 text-blue-600 hover:border-blue-600 hover:text-blue-600'
+                                    : 'bg-gray-100 text-gray-400 hover:border-gray-600 hover:text-gray-600'
+                            }`}
                             onClick={() => setWeather(selectedWeather)}
                         >
                             {selectedWeather}
@@ -87,7 +97,11 @@ function DiaryWriter() {
                 maxLength={1000}
             />
             <button
-                className={`flex items-center justify-center rounded-lg border border-transparent active:translate-y-[1px] w-full p-2 cursor-pointer transition-colors ease-in ${isValid ? 'bg-emerald-100 text-emerald-600 hover:border-emerald-600 hover:text-emerald-600' : 'bg-gray-100 text-gray-400 hover:border-gray-600 hover:text-gray-600'}`}
+                className={`flex items-center justify-center rounded-lg border border-transparent active:translate-y-[1px] w-full p-2 cursor-pointer transition-colors ease-in ${
+                    isValid
+                        ? 'bg-emerald-100 text-emerald-600 hover:border-emerald-600 hover:text-emerald-600'
+                        : 'bg-gray-100 text-gray-400 hover:border-gray-600 hover:text-gray-600'
+                }`}
                 disabled={!isValid}
                 onClick={saveDiary}
             >
@@ -98,6 +112,7 @@ function DiaryWriter() {
 }
 
 function DiaryRecord() {
+    const diaries = useDiaryValue()
     const updateDiaries = useDiaryUpdate()
 
     useEffect(() => {
@@ -108,7 +123,33 @@ function DiaryRecord() {
     return (
         <div className="w-full flex flex-col items-start gap-4 p-4 justify-between rounded-lg bg-white border border-gray-100 h-2/3 min-h-[20rem]">
             <h1 className="text-xl mt-5 text-emerald-600">기록된 일기</h1>
-            <div className="flex items-center justify-center text-gray-400">일기를 적어보세요</div>
+            {diaries.length > 0 ? (
+                <div className="flex flex-col overflow-y-auto gap-2 w-full max-h-96">
+                    {diaries.map((diary) => (
+                        <Link
+                            to={`/detail/${diary.id}`}
+                            className="flex flex-col items-start justify-center gap-1.5 p-3 hover:bg-gray-50 trasition cursor-pointer border border-gray-100 rounded-lg"
+                        >
+                            <h1 className="text-base text-gray-800">{diary.title}</h1>
+                            <div className="flex flex-row items-center justify-between gap-1 w-full">
+                                <span className="text-gray-400 text-sm">
+                                    {new Date(diary.date).toLocaleDateString()}
+                                </span>
+                                <div className="flex flex-row gap-1s">
+                                    <div className="w-6 h-6 p-1 flex items-center justify-center rounded-full border border-gray-100">
+                                        {EMOTION[diary.emotion].emoji}
+                                    </div>
+                                    <div className="w-6 h-6 p-1 flex items-center justify-center rounded-full border border-gray-100">
+                                        {WEATHER[diary.weather]}
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            ) : (
+                <div className="flex items-center justify-center text-gray-400">일기를 적어보세요</div>
+            )}
             <Link
                 to="/emotions"
                 className="flex items-center justify-center rounded-lg border border-transparent active:translate-y-[1px] w-full p-2 cursor-pointer transition-colors ease-in bg-emerald-100 text-emerald-600 hover:border-emerald-600 hover:text-emerald-600"
