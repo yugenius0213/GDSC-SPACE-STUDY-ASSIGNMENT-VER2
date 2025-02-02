2cc92bcff0057e587c02838af939afc614350416
chae1xx1os <kty1895@naver.com>
2025-02-02 14:46:12 +0900

    fix: add icon detail and fix order (#34)

--- Taeyang/react-ts/src/constants/emotions.ts
+++ Taeyang/react-ts/src/constants/emotions.ts
@@ -2,6 +2,40 @@ export type EmotionTextType = {
   title: string;
   desc: string;
   color: 'red' | 'yellow' | 'green' | 'blue' | 'purple';
+  icon: string;
+}
+
+export const emotionTexts: Record<string, EmotionTextType> = {
+  awesome: {
+    title: 'Awesome',
+    desc: '최고의 하루였어요',
+    color: 'yellow',
+    icon: '😎',
+  },
+  great: {
+    title: 'Great',
+    desc: '멋진 하루였어요',
+    color: 'blue',
+    icon: '😃',
+  },
+  good: {
+    title: 'Good',
+    desc: '좋은 하루였어요',
+    color: 'green',
+    icon: '😙',
+  },
+  soso: {
+    title: 'Soso',
+    desc: '괜찮은 하루였어요',
+    color: 'purple',
+    icon: '😗',
+  },
+  bad: {
+    title: 'Bad',
+    desc: '최악의 하루였어요!',
+    color: 'red',
+    icon: '🤬',
+  },
 }
 
 export const emotions = [
@@ -11,31 +45,3 @@ export const emotions = [
   { label: 'soso', icon: '😗' },
   { label: 'bad', icon: '🤬' },
 ];
-
-export const emotionTexts: Record<string, EmotionTextType> = {
-  awesome: {
-    title: 'Awesome',
-    desc: '최고의 하루였어요',
-    color: 'yellow',
-  },
-  great: {
-    title: 'Great',
-    desc: '멋진 하루였어요',
-    color: 'blue',
-  },
-  good: {
-    title: 'Good',
-    desc: '좋은 하루였어요',
-    color: 'green',
-  },
-  soso: {
-    title: 'Soso',
-    desc: '괜찮은 하루였어요',
-    color: 'purple',
-  },
-  bad: {
-    title: 'Bad',
-    desc: '최악의 하루였어요!',
-    color: 'red',
-  },
-};
